f1777316a0354f73ac887a1f7aa99bb96abecc23
lfarant <lfarant01@gmail.com>
2018-02-05 18:20:53 +0100

    backend commit
    fully functionnal backend

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,20 +2,61 @@
 
 namespace App\Controller;
 
+use App\Form\MessageType;
+use App\Entity\Message;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\component\HttpFoundation\Session\Session;
 
 class HomeController extends Controller
 {
    
     /**
-     * @Route("/", name= "index")
+     * @Route("/", name="index")
      */
-	public function indexAction()
+    public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('App/index.html.twig');
+        $session = new Session();
+		$message = new message();
+		$form = $this->get('form.factory')->create(MessageType::class, $message);
+		
+		$form->handleRequest($request);
+		if ($form->isSubmitted() && $form->isValid()) {
+			
+			$em 			= $this->getDoctrine()->getManager();
+			$em->persist($message);
+            $em->flush();
+			
+			$session->getFlashBag()->add('success', 'Votre message à bien été envoyé');
+			$session->clear();
+			
+			return $this->redirectToRoute('message_view');
+		}
+		
+		
+		return $this->render('App/index.html.twig', ['form' => $form->createView()] );
+    }
+	
+	/**
+     * @Route("/messages", name="message_view")
+     */
+    public function messageViewAction(Request $request)
+    {
+		$repository = $this->getDoctrine()->getRepository(Message::class);
+		$message = new Message;
+		$messages = $repository->findAll();
+		/*
+		* messages display
+		*/
+		foreach ($messages as $message){
+			$message->getId();
+			$message->getName();
+			$message->getContent();
+			
+		}
+		
+		return $this->render('Messages/index.html.twig', ['messages' => $messages,] );
     }
 }
---
+++ src/Entity/Message.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MessageRepository")
+ */
+class Message
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer", name="id")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", unique=false)
+     * @Assert\NotBlank(message="Have you forgot to enter your name ?")
+     */
+	private $name;
+	
+	/**
+     * @var string
+     *
+     * @ORM\Column(type="string", unique=false)
+     * @Assert\NotBlank(message="Have you forgot to enter your email ?")
+     * @Assert\Email()
+     */
+	private $email;
+	
+	/**
+     * @var string
+     *
+     * @ORM\Column(type="string", unique=false)
+     * @Assert\NotBlank(message="Have you forgot to enter your message ?")
+     */
+	private $content;
+	
+	public function getId()
+	{
+		return $this->id;
+	}
+	
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
+	
+	public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+}
